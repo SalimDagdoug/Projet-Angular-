14d4b06166282dfc8882d549e621e577be5ed413
unknown <salimdagdou44@gmail.com>
2024-12-03 17:46:13 +0100

    service for add product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,42 +3,42 @@ import { HomeComponent } from './home/home.component';
 import { ProduitComponent } from './produit/produit.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StockComponent } from './stock/stock.component';
-import { StockdetailsComponent } from './stock/stockdetails/stockdetails.component';
+import { AjoutproduitComponent } from './ajoutproduit/ajoutproduit.component';
 
 export const routes: Routes = [
-    { 
-      path: '', 
-      redirectTo: 'Home', 
-      pathMatch: 'full' 
-    },
-    { 
-      path: 'Home',
-      component: HomeComponent,
-      children: [
-        {
-          path: '',
-          redirectTo: 'Dashboard', // Redirige vers "Dashboard" par défaut
-          pathMatch: 'full'
-        },
-        {
-          path: 'Dashboard', // Sous-route pour "Dashboard"
-          component: DashboardComponent, 
-          pathMatch: 'full'
-        },
-        {
-          path: 'Produit', // Sous-route pour "Produit"
-          component: ProduitComponent,
-        },
-        {
-          path: 'Stock', // Sous-route pour "Stock"
-          component: StockComponent,
-        },
-        {
-          path: 'Stock-details/:id', // Route pour afficher les détails du stock
-          component: StockdetailsComponent, // Affiche les détails du stock
-        }
-      ]
-    },
-    { path: '**', redirectTo: 'Home' } // Redirection pour les chemins non définis
-  ];
-  
+  { 
+    path: '', 
+    redirectTo: 'Home', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'Home',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'Dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'Dashboard',
+        component: DashboardComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'Produit',
+        component: ProduitComponent,
+        runGuardsAndResolvers: 'always',  // Forces re-initialization
+      },
+      {
+        path: 'Stock',
+        component: StockComponent,
+      },
+      {
+        path: 'AddProduit',
+        component: AjoutproduitComponent,
+      },
+    ]
+  },
+  { path: '**', redirectTo: 'Home' }
+];
